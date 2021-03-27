fa5d94e004566aa006bf032e8ebf5e963e822c03
Ricardo <44264239+Ricardoltao@users.noreply.github.com>
2021-03-27 16:49:43 +0800

    更改端口号

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-09 11:26:39
- * @LastEditTime: 2021-03-24 12:53:30
+ * @LastEditTime: 2021-03-27 16:49:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \blueSpace_server\controller\user.js
@@ -26,7 +26,7 @@ const uuid = require('node-uuid')
 class UserController {
   constructor() {
     this.secret = 'lee' // 定义签名
-    this.defaultAvatar = 'http://192.168.0.105:3000/avatar/lee.jpg'
+    this.defaultAvatar = 'http://8.129.214.128:3001/avatar/lee.jpg'
     this.login = this.login.bind(this)
     this.register = this.register.bind(this)
     this.getAccountInfo = this.getAccountInfo.bind(this)
