2e79b0af2bf63a33688507cbc7dd6c493bf7c09c
lee13060852786@163.com <lee13060852786@163.com>
2021-05-26 01:29:26 +0800

    提交

--- download.js
+++ download.js
@@ -1,33 +1,23 @@
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+//注意require('koa-router')返回的是函数:
+//设置跨域
+const cors = require('koa2-cors'); //跨域处理
+//导入post请求参数处理的插件
+const bodyParser = require('koa-bodyparser');
+// 引入静态资源中间件，静态web服务
+const staticResource = require('koa-static');
+// 引入node的path模块
+const path = require('path')
 var fs = require('fs');
-var http = require('http');
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+app.use(cors());
+// add router middleware:
+app.use(bodyParser());
+//配置静态资源中间件
+app.use(staticResource(path.join(__dirname,'download')))
 
-var PORT = 3000;
-
-createHttpServer();
-
-/*
- * Create http server.
- */
-function createHttpServer() {
-    http.createServer(function (req, res) {
-        downloadFile(res);
-    }).listen(PORT, 'localhost');
-    console.log('Server running at http://localhost:' + PORT + '...');
-}
-
-/*
- * Output the download file.
- */
-function downloadFile(res) {
-    // The must headers.
-    res.setHeader('Content-type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment;filename=app-release.apk');    // 'aaa.txt' can be customized.
-    var fileStream = fs.createReadStream('./download/app-release.apk');
-    fileStream.on('data', function (data) {
-        res.write(data, 'binary');
-    });
-    fileStream.on('end', function () {
-        res.end();
-        console.log('The file has been downloaded successfully!');
-    });
-}
+// 在端口3000监听:
+app.listen(3000);
+console.log('服务器创建储成功，启动的端口是3000');
